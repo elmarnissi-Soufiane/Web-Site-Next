518b32414593ffa625110db06bded12483012f1a
Elmarnissi soufiane <soufiane.elmarnissi@edu.uiz.ac.ma>
2024-04-19 09:23:49 +0000

    Add Folder Cont and Change Public Folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Link from "next/link"
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
-        <div> Nav Bar </div>
+        <nav className='flexBetween max-container padding-container relative z-30 py-5'>
+            <Link href="/">
+                <Image src="/hilink-logo.svg" alt='logo' width={74} height={29}/>
+
+                <ul className='hidden h-full gap-12 lg:flex'></ul>
+
+            </Link>
+        </nav>
     );
 }
 export default Navbar
